1576371dd1533ac19c92b5ad0b8e5f0b2f0d720f
Alexander-Yershov <alex04398956@gmail.com>
2021-11-17 15:21:56 +0100

    завершает дз № 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStartRef = document.querySelector('button[data-start]')
+const btnStopRef = document.querySelector('button[data-stop]');
+let timerId = null;
 
+btnStopRef.setAttribute('disabled','disabled');
+
+btnStartRef.addEventListener("click", () => {
+    timerId = setInterval(() => {document.body.style.background = getRandomHexColor()}, 1000);
+    btnStartRef.setAttribute('disabled','disabled');
+    btnStopRef.removeAttribute('disabled');
+    
+  });
+
+btnStopRef.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStopRef.setAttribute('disabled','disabled');
+    btnStartRef.removeAttribute('disabled');
+  });
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
